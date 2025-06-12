73addbe0fb83a6f4d1dda7ec36d37e3ab07d5a4e
juannquinones <130688961+juannquinones@users.noreply.github.com>
2025-06-12 12:06:40 -0500

    use variable search_on on the query

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,9 +37,9 @@ if ingredients_list:
         ingredients_string += i+' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', i,' is ', search_on, '.')
+        #st.write('The search value for ', i,' is ', search_on, '.')
         st.subheader(i+'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
 
     #st.write(ingredients_string)
