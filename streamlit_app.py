7957bf42b3504b54be2e61ae34f8927b5b4ee7aa
juannquinones <130688961+juannquinones@users.noreply.github.com>
2025-06-12 12:03:35 -0500

    better search

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 
 ingredients_list= st.multiselect('Choose up to 5 ingredients: ', 
                                  my_dataframe, max_selections = 5)
@@ -36,6 +35,9 @@ if ingredients_list:
     ingredients_string =''
     for i in ingredients_list:
         ingredients_string += i+' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(i+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
